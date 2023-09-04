be6500f1bd7bbb5985ba5a475b3e2f29688346cc
hanhosi <leehanbyeols2@gmail.com>
2023-09-04 18:30:34 +0900

    first commit

--- editest.py
+++ editest.py
@@ -1,5 +1,16 @@
 a = "hello"
 b = "world"
-print(a. b)
+print(a, b)
 
 print(a)
+print(b)
+print(b, a)
+print("python", a, b)
+number_school = 2394584903
+str_name = "pyhton"
+bool_view = False
+flt_pi = 3.14
+char_element = 'a'
+chars = "hello world"
+print("number : ", number_school, " name : ", str_name, " bool : ", bool_view, " float : ", flt_pi, " charter : " , char_element, " charcters : ", chars)
+111
